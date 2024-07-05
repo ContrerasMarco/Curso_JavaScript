b3b0f523356bf76b3296f521816196f1228eb47e
Marco Contreras <mcontreras42@outlook.com>
2024-07-05 18:17:09 -0400

    Entendiendo declaracion reasignacion y re declaracion de una variable

---
+++ VariablesReasignacionRedeclaracion.js
@@ -0,0 +1,30 @@
+//declarar, reasignar y redeclarar con VAR.
+var firstname; //inicializamos la variable
+firstname = "marco"; //asignamos un valor a la variable
+console.log(firstname); //accedemos a la variable.
+
+var lastname = "contreras"; //asignamos y declaramos en una linea la variable
+lastname = "aranda"; //reasinagmos la variable
+console.log(lastname); // accedimos a ella
+
+//declarar, reasignar y redeclarar con LET
+
+let frutas = "Mora"; // declaramos y asignamos
+frutas = "kiwi"; //con LET puedo reasignar el valor de una variable.
+console.log(frutas);
+
+//let frutas = "manzana"; // con LET no podemos re declarar una variable
+
+/*en resumen con LET podemos declarar y asignar en una misma linea, ademas
+de reasignar la variable pero no podemos re declarar una variable*/
+
+//Declarar, reasignar y re declarar con CONST
+
+const team = "U de chile"; //declarar y asignar
+console.log(team); //accedemos a la variable y nos imprime la variable
+
+team = "CC"; //reasinagmos pero con const no es posible reasignar una variable pro su scope de bloque
+console.log(team);
+
+const team = "UC"; //re declarar tampoco podemos con const.
+console.log(team);
