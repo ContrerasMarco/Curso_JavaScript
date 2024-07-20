6729d4518659b7d177c97574a3d952b3703055a7
Marco Contreras <mcontreras42@outlook.com>
2024-07-20 00:29:12 -0400

    actualizando archivo de operadores

---
+++ OperadoresDeComparacion.js
@@ -0,0 +1,14 @@
+const a = 10;
+const b = 20;
+const c = "10";
+const d = "10";
+
+console.log(a == b); //Con "==" Compara si el valor es igual
+console.log(a === b); //compara si el valo y el tipo de dato son iguales
+console.log(a == c);
+console.log(a === c);
+console.log(c === d);
+
+/*otros tipos de operadores de comparacion son el mayo, el menor o mayor o menor igual
+tambien la negacion simple de comparar los datos != o ese mismo pero comparando el valor y
+el tipo de dato con el !==*/
